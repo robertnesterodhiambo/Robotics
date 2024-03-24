2b973dbeb00802ea6d43f2b1e2810991117581cd
robert nester odhiambo <robertnesterodhiambo@gmail.com>
2024-03-24 14:39:43 +0300

    my first node

--- src/my_robot_controller/my_robot_controller/my_first_node.py
+++ src/my_robot_controller/my_robot_controller/my_first_node.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env pyhton3
 import rclpy
+from rclpy.node import Node
+
+
+class Mynode(Node):
+    
+    def __init__(self):
+        super().__init__("first_node")
+        self.get_logger().info("hello from ROS2")
+
+def main(args = None):
+    rclpy.init(args = args) # installing node with ros2 functionalities
+    ## node goese in here
+    ## we use oop 
+    node  = Mynode()
+    
+    
+    rclpy.shutdown()
+if __name__ == '__main__':
+    main()
