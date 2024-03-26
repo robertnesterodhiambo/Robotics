c2f280523d0c5d8d72225da9f742ee44b3c36aa7
robert nester odhiambo <robertnesterodhiambo@gmail.com>
2024-03-26 11:26:28 +0300

    turtlecontroler

---
+++ src/my_robot_controller/my_robot_controller/turtle_controller.py
@@ -0,0 +1,16 @@
+#!/usr/bin/envr python3
+import rclpy
+from rclpy.node import Node
+
+class TurtleControllerNode(Node):
+    def __init__(self):
+        super().__init__("turtle_controller")
+        self.get_logger().info("Turtle controller has been started")    
+    
+
+
+def main(args=None):
+    rclpy.init(args = args)
+    node = TurtleControllerNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
